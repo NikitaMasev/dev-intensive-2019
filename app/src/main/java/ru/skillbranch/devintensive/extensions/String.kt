088fae2a10a4f951e05c959632bfab9060df4731
Nikita Masev <Nikita.Masev@gmail.com>
2019-07-06 13:46:30 +0300

    DTO User, Factory Base Message, AbstractFactory parseFullName Date.format Date.add *toInitials *transliteration **Паттерн Builder *Date.humanizeDiff **plural *String.truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,12 @@
 package ru.skillbranch.devintensive.extensions
 
+fun String.truncate(value: Int = 16): String {
+    val payload = "..."
+    val newStr = this.trim()
+
+    return when {
+        newStr.length > value -> newStr.substring(0, value).trim().plus(payload)
+        else -> newStr
+    }
+}
+
